a2139861faedde91166243bb9599f85a58218a6a
Ethan Fregene <ethanfregene475@gmail.com>
2026-09-11 11:43:56 -0700

    initial spring boot setup using spring initalizer

---
+++ src/main/java/com/ethan/crypto_portfolio/CryptoPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.crypto_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPortfolioApplication.class, args);
+	}
+
+}
